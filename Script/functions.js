db6423f24e1e66bcc2e49157326cedbaf1bc720e
PrebenS <prebens.kodehode@gmail.com>
2023-09-21 15:25:18 +0200

    refactor

--- Script/functions.js
+++ Script/functions.js
@@ -1,27 +1,39 @@
-export function appendSVG(element, url) {
+export function appendSVG(element, url)  {
     return fetch(url)
-        .then((response) => response.text())
-        .then((svgContent) => {
-        const svgDocument = new DOMParser().parseFromString(svgContent, "image/svg+xml");
+      .then((response) => response.text())
+      .then((svgContent) => {
+        const svgDocument = new DOMParser().parseFromString(
+          svgContent,
+          "image/svg+xml"
+        );
         const svgElement = svgDocument.documentElement;
         element.append(svgElement);
-    })
-        .catch((error) => {
+      })
+      .catch((error) => {
         console.error("Error loading SVG:", error);
-    });
-}
-/**
- * update classes of a DOM element by adding or removing specific class names.
- *
- * @param {Element} element - the DOM element to update.
- * @param {string[]} [add=[]] - an array of class names to add to the element.
- * @param {string[]} [remove=[]] - an array of class names to remove from the element.
- * @returns {void}
- */
-export function updateClasses(element, add = [], remove = []) {
+      });
+  }
+  
+  /**
+   * update classes of a DOM element by adding or removing specific class names.
+   *
+   * @param {Element} element - the DOM element to update.
+   * @param {string[]} [add=[]] - an array of class names to add to the element.
+   * @param {string[]} [remove=[]] - an array of class names to remove from the element.
+   * @returns {void}
+   */
+  export function updateClasses(
+    element,
+    add = [],
+    remove = []
+  ) {
     add.forEach((cls) => element.classList.add(cls));
     remove.forEach((cls) => element.classList.remove(cls));
-}
-export function delay(ms) {
+  }
+  
+  export function delay(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
-}
+  }
+  
+  
+  
--- Script/script.js
+++ Script/script.js
@@ -1,52 +1,48 @@
 import { appendSVG, updateClasses } from "./functions.js";
+
 const menuBtn = document.querySelector("#ham-menu");
-let pathA = null;
-let pathB = null;
-let pathC = null;
-let hamburger = null;
-if (menuBtn instanceof HTMLElement) {
-    appendSVG(menuBtn, "../images/svg/hamburger_menu.svg").then(() => {
-        pathA = document.querySelector("#pathA");
-        pathB = document.querySelector("#pathB");
-        pathC = document.querySelector("#pathC");
-        hamburger = document.querySelector("#hamburger");
-    });
-}
-else {
-    console.error("Menu button not found");
-}
+
+appendSVG(menuBtn, "../images/svg/hamburger_menu.svg").then(() => {
+  pathA = document.querySelector("#pathA");
+  pathB = document.querySelector("#pathB");
+  pathC = document.querySelector("#pathC");
+  hamburger = document.querySelector("#hamburger");
+});
+
 let isMenuOpen = false;
-menuBtn?.addEventListener("click", () => {
-    if (isMenuOpen === false) {
-        if (pathA instanceof SVGElement) {
-            updateClasses(pathA, ["pathA-to-open"], ["pathA-start", "pathA-to-closed"]);
-        }
-        if (pathB instanceof SVGElement) {
-            updateClasses(pathB, ["pathB-to-open"], ["pathB-start", "pathB-to-closed"]);
-        }
-        if (pathC instanceof SVGElement) {
-            updateClasses(pathC, ["pathC-to-open"], ["pathC-start", "pathC-to-closed"]);
-        }
-        if (hamburger instanceof SVGElement) {
-            updateClasses(hamburger, ["hamburger-open"], ["hamburger-closed"]);
-        }
-        isMenuOpen = true;
+menuBtn.addEventListener("click", () => {
+  if (isMenuOpen === false) {
+    updateClasses(pathA, ["pathA-to-open"], ["pathA-start", "pathA-to-closed"]);
+
+    updateClasses(pathB, ["pathB-to-open"], ["pathB-start", "pathB-to-closed"]);
+
+    updateClasses(pathC, ["pathC-to-open"], ["pathC-start", "pathC-to-closed"]);
+
+    updateClasses(hamburger, ["hamburger-open"], ["hamburger-closed"]);
+
+    isMenuOpen = true;
+  } else {
+    if (isMenuOpen === true) {
+      updateClasses(
+        pathA,
+        ["pathA-to-closed"],
+        ["pathA-start", "pathA-to-open"]
+      );
+
+      updateClasses(
+        pathB,
+        ["pathB-to-closed"],
+        ["pathB-start", "pathB-to-open"]
+      );
+
+      updateClasses(
+        pathC,
+        ["pathC-to-closed"],
+        ["pathC-start", "pathC-to-open"]
+      );
+
+      updateClasses(hamburger, ["hamburger-closed"], ["hamburger-open"]);
     }
-    else {
-        if (isMenuOpen === true) {
-            if (pathA instanceof SVGElement) {
-                updateClasses(pathA, ["pathA-to-closed"], ["pathA-start", "pathA-to-open"]);
-            }
-            if (pathB instanceof SVGElement) {
-                updateClasses(pathB, ["pathB-to-closed"], ["pathB-start", "pathB-to-open"]);
-            }
-            if (pathC instanceof SVGElement) {
-                updateClasses(pathC, ["pathC-to-closed"], ["pathC-start", "pathC-to-open"]);
-            }
-            if (hamburger instanceof SVGElement) {
-                updateClasses(hamburger, ["hamburger-closed"], ["hamburger-open"]);
-            }
-            isMenuOpen = false;
-        }
-    }
+    isMenuOpen = false;
+  }
 });
