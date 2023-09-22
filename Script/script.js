d7e5c1ca3077b0d467c207b739dfa017341ccbad
PrebenS <prebens.kodehode@gmail.com>
2023-09-22 16:26:04 +0200

    styling

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,8 @@
-import { appendSVG, updateClasses } from "./functions.js";
+import { appendSVG, updateClasses, delay } from "./functions.js";
 
 const menuBtn = document.querySelector("#ham-menu");
+const menu = document.querySelector("nav");
+const menuListItem = document.querySelectorAll(".menu-link");
 
 appendSVG(menuBtn, "../images/svg/hamburger_menu.svg").then(() => {
   pathA = document.querySelector("#pathA");
@@ -10,7 +12,8 @@ appendSVG(menuBtn, "../images/svg/hamburger_menu.svg").then(() => {
 });
 
 let isMenuOpen = false;
-menuBtn.addEventListener("click", () => {
+
+menuBtn.addEventListener("click", async () => {
   if (isMenuOpen === false) {
     updateClasses(pathA, ["pathA-to-open"], ["pathA-start", "pathA-to-closed"]);
 
@@ -44,5 +47,20 @@ menuBtn.addEventListener("click", () => {
       updateClasses(hamburger, ["hamburger-closed"], ["hamburger-open"]);
     }
     isMenuOpen = false;
+  }
+  if (isMenuOpen) {
+    updateClasses(menu, ["right_fade_in"], ["right_fade_out"]);
+    await delay(100);
+    menuListItem.forEach(async (item, i) => {
+      await delay(100 * i);
+      updateClasses(item, ["right_fade_in"], ["right_fade_out"]);
+    });
+  } else {
+    menuListItem.forEach(async (item, i) => {
+      await delay(100 * i);
+      updateClasses(item, ["right_fade_out"], ["right_fade_in"]);
+    });
+    await delay(500);
+    updateClasses(menu, ["right_fade_out"], ["right_fade_in"]);
   }
 });
